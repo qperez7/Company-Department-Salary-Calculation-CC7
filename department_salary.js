f7199c8211ea7ec9d95be4699cc6429496bc79e0
Quirianyailet Perez <qperez1@usf.edu>
2024-10-03 15:18:54 -0400

    update1

--- department_salary.js
+++ department_salary.js
@@ -61,14 +61,25 @@ const company = {
 console.log(company);
 
  //Task 2: Create a Recursive Function to Calculate Total Salary for a Department
-function calculateDepartmentSalary (department) {
-let companyDepartments = company.departments.department
-let totalDepartmentSales = companyDepartments.find((employees) => employees.salary)       
-        
+function calculateDepartmentSalary (departmentName) {
+    let companyEmployees = company.employees
+    company.departments.forEach((departmentName))
+      if(Array.isArray(employees)) {
+        return employees.reduce((prev, current) => prev + current.salary, 0)
+      }
+        let sum = 0
+        for(let subordinate of companyEmployees.subordinates ) {
+            sum += calculateDepartmentSalary(subordinate);
+        }
+        return sum
+    
+    } console.log(calculateDepartmentSalary('Engineering'))
+
+
  //Recursively calculate the sales for each subordinate
 for( let subordinate of department.subordinates) {
     totalDepartmentSales += calculateDepartmentSalary(subordinate);
-}
+
 return totalDepartmentSales;
 }
 //Calculate total salary for a department
@@ -76,14 +87,14 @@ const totalDepartmentSales = calculateDepartmentSalary('Enginnering');
 console.log(`Total salary for the Enginnering department: $${totalDepartmentSales}`);
 
 // Task 3: Create a Function to Calculate the Total Salary for All Departments
-//function calculateDepartmentSalary(company) {
-    //let totalSalary = company.departments.employees.salary;
+function calculateDepartmentSalary(company) {
+let totalSalary = employees.reduce((prev,current) => prev + current.salary, 0); 
 // Recursive calculations for salary for each subordinate
-//for( let subordinate of company.subordinates) {
-    //totalSalary += calculateDepartmentSalary(subordinate);
-//}
-//return totalSalary;
-//}
+    for( let subordinate of company.subordinates) {
+    totalSalary += calculateDepartmentSalary(subordinate);
+}
+return totalSalary;
+};
 // Calculate total salary for the company
-//const totalSalary = calculateDepartmentSalary(company);
-//console.log(`Total Sales for the company: $${totalSalary}`); 
+const totalSalary = calculateDepartmentSalary(company);
+console.log(`Total Sales for the company: $${totalSalary}`); 
