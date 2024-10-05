d57f2655aa37d1e8466f6579e2e285ff369cad0f
Quirianyailet Perez <qperez1@usf.edu>
2024-10-04 20:00:16 -0400

    update 2

--- department_salary.js
+++ department_salary.js
@@ -61,40 +61,30 @@ const company = {
 console.log(company);
 
  //Task 2: Create a Recursive Function to Calculate Total Salary for a Department
-function calculateDepartmentSalary (departmentName) {
-    let companyEmployees = company.employees
-    company.departments.forEach((departmentName))
-      if(Array.isArray(employees)) {
-        return employees.reduce((prev, current) => prev + current.salary, 0)
-      }
-        let sum = 0
-        for(let subordinate of companyEmployees.subordinates ) {
-            sum += calculateDepartmentSalary(subordinate);
-        }
-        return sum
-    
-    } console.log(calculateDepartmentSalary('Engineering'))
+function calculateDepartmentSalary (department) {
+    let sum = 0
+function calculateEmployeeSalary (employee) {
+    sum += employee.salary;
 
-
- //Recursively calculate the sales for each subordinate
-for( let subordinate of department.subordinates) {
-    totalDepartmentSales += calculateDepartmentSalary(subordinate);
-
-return totalDepartmentSales;
-}
-//Calculate total salary for a department
-const totalDepartmentSales = calculateDepartmentSalary('Enginnering');
-console.log(`Total salary for the Enginnering department: $${totalDepartmentSales}`);
+    //Recursively calculate the salary of each subordinate
+    if (employee.subordinates.length > 0) {
+    for(let subordinate of employee.subordinates) {
+        calculateEmployeeSalary(subordinate)
+    }}
+    //Recursively calculate the salary of each employee
+    for (let employee of department.employees ) {
+        calculateEmployeeSalary(employee)
+    return sum
+}}}
+const sum = calculateDepartmentSalary('Engineering');
+console.log(`Total salary for the Enginnering department: $${sum}`);
 
 // Task 3: Create a Function to Calculate the Total Salary for All Departments
-function calculateDepartmentSalary(company) {
-let totalSalary = employees.reduce((prev,current) => prev + current.salary, 0); 
-// Recursive calculations for salary for each subordinate
-    for( let subordinate of company.subordinates) {
-    totalSalary += calculateDepartmentSalary(subordinate);
-}
+function calculateCompanySalary() {
+for (let company of company.departments) {
+    calculateDepartmentSalary(company)
 return totalSalary;
-};
-// Calculate total salary for the company
+}}
+//Calculate total salary for the company
 const totalSalary = calculateDepartmentSalary(company);
-console.log(`Total Sales for the company: $${totalSalary}`); 
+console.log(`Total salary for the Company: $${totalSalary}`); 
